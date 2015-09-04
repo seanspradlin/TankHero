5b4ad3ff1ef028f4df97b7f02752c37d2134dccd
Sean Spradlin <seanspradlin@gmail.com>
2015-09-03 21:45:59 -0500

    remove old scripts

--- app/scripts/States/preload.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-function PreloadState() {
-  this.greet();
-}
--- app/scripts/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var game = new Phaser.Game(1200, 800, Phaser.AUTO, 'game');
-game.state.add('main', MainState);
-game.state.start('main');
