91f12337964c877e3e4c8c86b16f9493c81dc1e7
Sean Spradlin <seanspradlin@gmail.com>
2015-09-05 13:55:09 -0500

    Created bomber with animations

---
+++ app/scripts/entities/bomber.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Bomber.prototype = Object.create(Phaser.Sprite.prototype);
+Bomber.prototype.constructor = Bomber;
+
+function Bomber(game, x, y) {
+
+  // Call base constructor
+  Phaser.Sprite.call(this, game, x, y, 'sprites', 'bomber/body');
+
+  // Propeller
+  var propeller = new Phaser.Sprite(this.game, 8, 47, 'sprites', 'bomber/propeller1');
+  propeller.anchor.x = 1.0;
+  propeller.anchor.y = 0.5;
+  var spinningFrames = Phaser.Animation.generateFrameNames('bomber/propeller', 1, 4);
+  propeller.animations.add('spinning', spinningFrames, 30, true, false);
+  propeller.animations.play('spinning');
+  this.addChild(propeller);
+}
