f5a692c7ee0bc3cd60c8e30a6ce3ef78861f244c
Sean Spradlin <seanspradlin@gmail.com>
2015-09-06 22:36:04 -0500

    Added cannon to player

--- app/scripts/entities/player.js
+++ app/scripts/entities/player.js
@@ -3,19 +3,32 @@ Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
 
 function Player(game, x, y) {
+  var self = this;
+
   // Call base constructor
   Phaser.Sprite.call(this, game, x, y, 'sprites', 'player/body1');
 
   // Animations
   var forwardFrames = Phaser.Animation.generateFrameNames('player/body', 1, 6);
-  this.animations.add('forward', forwardFrames, 8, true, false);
+  this.animations.add('forward', forwardFrames, 12, true, false);
 
   var reverseFrames = Phaser.Animation.generateFrameNames('player/body', 6, 1);
-  this.animations.add('reverse', reverseFrames, 8, true, false);
+  this.animations.add('reverse', reverseFrames, 12, true, false);
 
   // Anchor
   this.anchor.x = 0.5;
   this.anchor.y = 1.0;
+
+  var cannon = new Phaser.Sprite(this.game, -8, -36, 'sprites', 'player/cannon1');
+  var firingFrames = Phaser.Animation.generateFrameNames('player/cannon', 1, 4);
+  cannon.anchor.x = 0.0;
+  cannon.anchor.y = 0.5;
+  cannon.scale.x = -2.0;
+  cannon.animations.add('firing', firingFrames, 10, false, false);
+  cannon.events.onAnimationComplete.add(function() {
+    self.cannon.animations.previous(firingFrames.length);
+  });
+  this.cannon = this.addChild(cannon);
 }
 
 Player.prototype.forward = function() {
@@ -31,5 +44,5 @@ Player.prototype.halt = function() {
 };
 
 Player.prototype.attack = function() {
-
+  this.cannon.animations.play('firing');
 };
