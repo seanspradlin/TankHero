9176a4020d88a3db6420d3457d3e70bf65d564ae
Sean Spradlin <seanspradlin@gmail.com>
2015-09-08 21:47:14 -0500

    created shell.js

---
+++ app/scripts/entities/shell.js
@@ -0,0 +1,20 @@
+'use strict';
+Shell.prototype = Object.create(Phaser.Sprite.prototype);
+Shell.prototype.constructor = Shell;
+
+function Shell(game, key) {
+  Phaser.Sprite.call(this, game, 0, 0, 'sprites', key);
+  this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+  this.anchor.set(0.5);
+  this.outOfBoundsKill = true;
+  this.checkWorldBounds = true;
+  this.tracking = false;
+  this.scaleSpeed = 0;
+  this.scale.x = 2;
+  this.scale.y = 2;
+  this.exists = false;
+
+  this.events.onKilled.add(function() {
+    // play explosion
+  }, this);
+}
