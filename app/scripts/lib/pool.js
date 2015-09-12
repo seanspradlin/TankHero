954d883d5dbf8a3652e2d1ed85dfa2d32c952ca9
Sean Spradlin <seanspradlin@gmail.com>
2015-09-12 13:49:40 -0500

    added bombs and bombExplosions to Pool

--- app/scripts/lib/pool.js
+++ app/scripts/lib/pool.js
@@ -2,11 +2,31 @@
 
 var Pool = (function() {
   var instance;
+  var game;
 
-  return function() {
+  return function(sourceGame) {
     if (!instance) {
-      instance = {};
+      game = sourceGame;
+      instance = populate();
     }
     return instance;
   };
+
+  function populate() {
+    var container = {};
+
+    // Bombs
+    container.bombs = new Phaser.Group(game, game.world, 'Bombs', false, true, Phaser.Physics.ARCADE);
+    for (var i = 0; i < 10; i++) {
+      container.bombs.add(new Bomb(game), true);
+    }
+
+    // Bomb Explosions
+    container.bombExplosions = new Phaser.Group(game, game.world, 'Bomb Explosions', false, false);
+    for (var i = 0; i < 10; i++) {
+      container.bombExplosions.add(new Explosion(game), true);
+    }
+
+    return container;
+  }
 }());
