b392790113a6d23686dc86fa4466954bcd9e6619
Sean Spradlin <seanspradlin@gmail.com>
2015-09-12 21:30:08 -0500

    add bombers to pool

--- app/scripts/lib/pool.js
+++ app/scripts/lib/pool.js
@@ -27,6 +27,19 @@ var Pool = (function() {
       container.bombExplosions.add(new Explosion(game), true);
     }
 
+    // Bombers
+    container.bombers = new Phaser.Group(game, game.world, 'Bombers', false);
+    for (var i = 0; i < 4; i++) {
+      var flip    = i % 1 === 0
+        , scale   = flip ? -1 : 1
+        , x       = flip ? game.width * - 0.5 : game.width * 1.5
+        , y       = 200 + (200 * Math.random())
+        , bomber  = new Bomber(game, x, y);
+
+      bomber.scale.x = scale;
+      container.bombers.add(bomber);
+    }
+
     return container;
   }
 }());
