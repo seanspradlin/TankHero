c5f2fc26db0c91bc3fef04b592ffeb579ff3a587
Sean Spradlin <seanspradlin@gmail.com>
2015-09-13 20:58:09 -0500

    changed parameters on shell

--- app/scripts/entities/shell.js
+++ app/scripts/entities/shell.js
@@ -2,8 +2,8 @@
 Shell.prototype = Object.create(Phaser.Sprite.prototype);
 Shell.prototype.constructor = Shell;
 
-function Shell(game) {
-  Phaser.Sprite.call(this, game, 0, 0, 'sprites', 'player/shell');
+function Shell(game, key) {
+  Phaser.Sprite.call(this, game, 0, 0, 'sprites', key);
   this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
   this.anchor.set(0.5);
   this.outOfBoundsKill = true;
