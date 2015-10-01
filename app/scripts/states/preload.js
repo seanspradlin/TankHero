9f87c44a193394944b1c01f33c2a576b3e2a8d02
Sean Spradlin <seanspradlin@gmail.com>
2015-09-30 21:28:48 -0500

    updated to ogg

--- app/scripts/states/preload.js
+++ app/scripts/states/preload.js
@@ -14,14 +14,16 @@ Preload.preload = function() {
     'assets/images/sprites.png',
     'assets/images/sprites.json'
   );
-  this.load.audio('fire', 'assets/sounds/fire.mp3');
-  this.load.audio('panther-fire', 'assets/sounds/panther-fire.mp3');
-  this.load.audio('panther-explosion', 'assets/sounds/panther-explosion.mp3');
-  this.load.audio('jeep-grenade', 'assets/sounds/jeep-grenade.mp3');
-  this.load.audio('jeep-explosion', 'assets/sounds/jeep-explosion.mp3');
-  this.load.audio('bomb', 'assets/sounds/bomb.mp3');
-  this.load.audio('game-start', 'assets/sounds/game-start.mp3');
-  this.load.audio('game-over', 'assets/sounds/game-over.mp3');
+  this.load.audio('fire', 'assets/sounds/fire.ogg');
+  this.load.audio('panther-fire', 'assets/sounds/panther-fire.ogg');
+  this.load.audio('panther-explosion', 'assets/sounds/panther-explosion.ogg');
+  this.load.audio('jeep-grenade', 'assets/sounds/jeep-grenade.ogg');
+  this.load.audio('jeep-explosion', 'assets/sounds/jeep-explosion.ogg');
+  this.load.audio('bomb', 'assets/sounds/bomb.ogg');
+  // this.load.audio('propeller', 'assets/sounds/propeller.ogg');
+  this.load.audio('game-start', 'assets/sounds/game-start.ogg');
+  this.load.audio('game-over', 'assets/sounds/game-over.ogg');
+  this.load.audio('background', 'assets/sounds/background.ogg');
 };
 
 Preload.create = function() {
